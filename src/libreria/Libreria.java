94e08b1329b980af2c62b1dc2dcc02e969b8604f
aperezcesar <aperezcesar@nautilus1.danielcastelao.org>
2019-02-28 09:58:13 +0100

    notas

--- src/libreria/Libreria.java
+++ src/libreria/Libreria.java
@@ -5,8 +5,12 @@ import static libreria.IO.*;
 public class Libreria {
 
     public static void main(String[] args) {
+        //creamos dos objetos e indicamos cómo queremos que se muestren
         IO miIO = IO.introducir(VENTANA);
         miIO.mostrar("Hola");
+        
+        IO miIO2 = IO.introducir(CONSOLA);
+        miIO2.mostrar("Adios");
 
     }
 
